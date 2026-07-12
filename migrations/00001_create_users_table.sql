61527f25d1a328eb5237fe83fe93e4b77659d08c
Prajwal Patil <prajwalpatilk@gmail.com>
2026-07-12 12:37:15 +0530

    Update migrations

--- migrations/00001_create_users_table.sql
+++ migrations/00001_create_users_table.sql
@@ -1,12 +1,26 @@
 -- +goose Up
-CREATE TABLE users (
-    id uuid PRIMARY KEY DEFAULT uuidv7(),
+CREATE SCHEMA auth;
+
+CREATE TABLE auth.users (
+    id uuid DEFAULT uuidv7() PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
+
+CREATE TABLE auth.passwords (
+    id uuid REFERENCES auth.users(id) PRIMARY KEY,
+    encrypted_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE public.users (
+    id uuid REFERENCES auth.users(id) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP default current_timestamp,
-    is_active BOOLEAN DEFAULT true,
-    encrypted_password VARCHAR(255)
+    is_active BOOLEAN DEFAULT true
 );
 
 -- +goose Down
-DROP TABLE users;
+DROP TABLE public.users;
+DROP TABLE auth.passwords;
+DROP TABLE auth.users;
+
+DROP SCHEMA auth;
