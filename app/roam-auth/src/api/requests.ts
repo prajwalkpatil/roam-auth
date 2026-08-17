83fb0180a1b8edd4a72bedf1632e11271e22d3c7
Prajwal Patil <prajwalpatilk@gmail.com>
2026-08-17 22:40:02 +0530

    Refactor

--- app/roam-auth/src/api/requests.ts
+++ app/roam-auth/src/api/requests.ts
@@ -3,9 +3,7 @@ import type { AxiosResponse } from "axios"
 
 const BASE_URL = "http://localhost:8000"
 
-const ping: () => Promise<AxiosResponse> = function () {
+export function ping(): Promise<AxiosResponse> {
   const url = new URL(BASE_URL)
   return axios.get(url.toString())
 }
-
-export { ping }
