dfcb926b615f7a6c489167a61bcfabfcba6c960f
Prajwal Patil <prajwalpatilk@gmail.com>
2026-08-31 20:12:19 +0530

    Refactor

--- app/roam-auth/src/api/requests.ts
+++ app/roam-auth/src/api/requests.ts
@@ -26,7 +26,6 @@ api.interceptors.response.use(
       return Promise.reject(error)
     }
     config._retryCount = config._retryCount ? config._retryCount + 1 : 1
-    console.log("config._retryCount :>> ", config._retryCount)
     try {
       const response = await refresh()
       const data = response?.data as LoginResponse
