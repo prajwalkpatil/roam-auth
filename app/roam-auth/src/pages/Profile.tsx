8f82e7824df3dcfb46eceb82f69f78c118266d0c
Prajwal Patil <prajwalpatilk@gmail.com>
2026-08-31 21:14:40 +0530

    Refactor profile page

--- app/roam-auth/src/pages/Profile.tsx
+++ app/roam-auth/src/pages/Profile.tsx
@@ -1,11 +1,20 @@
 import { ping } from "@/api/requests"
 import AlertWrapper from "@/components/AlertWrapper"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Spinner } from "@/components/ui/spinner"
 import type { AxiosError } from "axios"
 import { useEffect, useState } from "react"
 
 export default function Profile() {
-  const [message, setMessage] = useState("")
+  const [, setMessage] = useState("")
   const [error, setError] = useState("")
   const [fetched, setFetched] = useState(false)
 
@@ -21,7 +30,42 @@ export default function Profile() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div>{!fetched && <Spinner />}</div>
-      <div>{message}</div>
+      {fetched && !error && (
+        <div className="flex w-3/4 md:w-1/2 lg:w-1/3 xl:w-2/7">
+          <Card className="flex-1">
+            <CardHeader>
+              <CardTitle>Profile</CardTitle>
+              <CardDescription>You’re now logged in.</CardDescription>
+            </CardHeader>
+            <CardContent className="mt-2">
+              <div className="flex flex-col rounded-md bg-accent px-3 py-2 text-[0.8rem]">
+                <div className="flex items-center justify-between gap-3 border-b pb-1.5">
+                  <div className="font-semibold text-muted-foreground">
+                    Name
+                  </div>
+                  <div className="overflow-y-auto text-end wrap-break-word text-secondary-foreground">
+                    Prajwal Patil
+                  </div>
+                </div>
+                <div className="flex items-center justify-between gap-3 pt-1.5">
+                  <div className="font-semibold text-muted-foreground">
+                    Email
+                  </div>
+                  <div className="overflow-y-auto text-end wrap-break-word text-secondary-foreground">
+                    prajwalpatilk@gmail.com
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button type="submit" className="w-full">
+                Logout
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      )}
+
       {error && (
         <AlertWrapper
           title="Something went wrong"
